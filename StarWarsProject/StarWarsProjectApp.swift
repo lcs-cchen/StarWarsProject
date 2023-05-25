29c9cb21467079a1feb2dea3fb00db00d4d2e330
cyrus chen <pschen@lcs.on.ca>
2023-05-25 14:43:00 -0400

    Initial Commit

---
+++ StarWarsProject/StarWarsProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsProjectApp.swift
+//  StarWarsProject
+//
+//  Created by Cyrus Chen on 25/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
